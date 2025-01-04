674f786096740d8a9031382ee94f444a711a7b26
Sri Naga Sri Govvala <37442887+SIRI1023@users.noreply.github.com>
2025-01-04 08:32:01 -0600

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -39,7 +39,7 @@ export function About() {
 
           <div className="mt-8">
             <a 
-              href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:965294d2-1d9a-44e7-8ab1-caa959158a16" 
+              href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:aad5ac0d-cf15-4c3e-aa35-43ffa0a540b8" 
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center space-x-2 bg-[#FFE169] text-gray-900 px-6 py-3 rounded-lg hover:bg-[#FFD93D] transition-colors"
